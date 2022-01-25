b04f54676759d879556ff2c4209463f1673fe788
Arihant Jain <jaina33@cardiff.ac.uk>
2022-01-25 05:22:39 +0000

    Comments/Rating feature added

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,9 @@ from blog import db, login_manager
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
+
+
 class Post(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
@@ -10,20 +13,25 @@ class Post(db.Model):
   content = db.Column(db.Text, nullable=False)
   image_file = db.Column(db.String(40), nullable=False, default='default.jpg')
   author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  comments = db.relationship('Comment', backref='post', lazy=True)
+  ratings = db.relationship('Rating', backref ='post', lazy=True)
 
   def __repr__(self):
     return f"Post('{self.date}', '{self.title}', '{self.content}')"
 
 class User(UserMixin,db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(15), unique=True, nullable=False)
+  first_name = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(20), unique= True, nullable=False)
   hashed_password=db.Column(db.String(128))
   post = db.relationship('Post', backref='user', lazy=True)
+  comments = db.relationship('Comment', backref='user', lazy=True)
+  ratings = db.relationship('Rating', backref ='user', lazy=True)
+
 
   def __repr__(self):
-    return f"User('{self.username}', '{self.email}')"
+    return f"User('{self.first_name}', '{self.email}')"
 
-#adated from Grinberg(2014, 2018)
   @property
   def password(self):
     raise AttributeError('Password is not readable.')
@@ -34,6 +42,21 @@ class User(UserMixin,db.Model):
 
   def verify_password(self,password):
     return check_password_hash(self.hashed_password,password)
+
+class Comment(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  content = db.Column(db.String(200), nullable =False)
+  date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  author = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+  post_id = db.Column(db.Integer,db.ForeignKey('post.id'), nullable = False)
+
+class Rating(db.Model):
+
+  stars = db.Column(db.Integer, nullable = False)
+  author= db.Column(db.Integer, db.ForeignKey('user.id'),primary_key = True)
+  post_id = db.Column(db.Integer, db.ForeignKey('post.id'),primary_key = True)
+
+  
 
 
 @login_manager.user_loader
--- blog/routes.py
+++ blog/routes.py
@@ -1,8 +1,9 @@
 from flask import render_template, url_for, request, redirect, flash
 from blog import app, db
-from blog.models import User, Post
-from blog.forms import RegistrationForm, LoginForm
-from flask_login import login_user, logout_user, current_user
+from blog.models import User, Post, Comment, Rating
+from blog.forms import RegistrationForm, LoginForm, CommentForm, RatingForm
+from flask_login import login_required, login_user, logout_user, current_user
+from sqlalchemy import exc
 
 @app.route("/")
 
@@ -18,37 +19,79 @@ def about():
 @app.route("/post/<int:post_id>")
 def post(post_id):
   post=Post.query.get_or_404(post_id)
-  return render_template('post.html',title=post.title,post=post)
+  form = CommentForm()
+  form_rating = RatingForm()
+  return render_template('post.html',title=post.title,post=post, form = form, form_rating= form_rating)
+
+@app.route("/post/<int:post_id>/comment", methods= ['GET','POST'])
+def addComment(post_id):
+  post=Post.query.get_or_404(post_id)
+  form = CommentForm()
+  form_rating = RatingForm()
+  if form.validate_on_submit():
+    if current_user.is_authenticated:
+      comment = Comment(content = form.content.data, author = current_user.id , post_id = post.id)
+      db.session.add(comment)
+      db.session.commit()
+      flash('Comment Added')
+      return redirect(url_for('post', post_id =post.id))
+    else:
+      flash('Please Login to comment')
+  return render_template('post.html',post=post, form = form, form_rating = form_rating)
+
+@app.route("/post/<int:post_id>/rating", methods= ['GET','POST'])
+def addRating(post_id):
+  post=Post.query.get_or_404(post_id)
+  form = CommentForm()
+  form_rating = RatingForm()
+  if form_rating.validate_on_submit():
+    if current_user.is_authenticated:
+      rate = Rating(stars = form_rating.stars.data, author = current_user.id , post_id = post.id)
+      try:
+        db.session.add(rate)
+        db.session.commit()
+        flash('Rating Added')
+      except exc.IntegrityError:
+        db.session.rollback()
+        flash('Cant give rating multiple times')
+      
+      return redirect(url_for('post', post_id =post.id))
+    else:
+      flash('Please Login to Rate')
+    
+  return render_template('post.html',post=post, form = form, form_rating = form_rating)
+
 
 @app.route("/register",methods=['GET','POST'])
 def register():
   form = RegistrationForm()
   if form.validate_on_submit():
-    user = User(username=form.username.data, password=form.password.data)
+    user = User(first_name= form.first_name.data, email = form.email.data, password=form.password.data)
     db.session.add(user)
     db.session.commit()
     flash('Registration successful!')
-    return redirect(url_for('registered'))
+    return redirect(url_for('home'))
   return render_template('register.html',title='Register',form=form)
 
-@app.route("/registered")
-def registered():
-  return render_template('registered.html', title='Thanks!')
 
 @app.route("/login",methods=['GET','POST'])
 def login():
   form = LoginForm()
   if form.validate_on_submit():
-    user = User.query.filter_by(username=form.username.data).first()
+    user = User.query.filter_by(email=form.email.data).first()
     if user is not None and user.verify_password(form.password.data):
       login_user(user)
-      flash('You\'ve successfully logged in,'+' '+ current_user.username +'!')
+      flash('You\'ve successfully logged in,'+' '+ current_user.first_name +'!')
       return redirect(url_for('home'))
-    flash('Invalid username or password.')
+    flash('Invalid email or password.')
   return render_template('login.html',title='Login', form=form)
 
 @app.route("/logout")
+@login_required
 def logout():
   logout_user()
   flash('You\'re now logged out. Thanks for your visit!')
   return redirect(url_for('home'))
+
+
+  
