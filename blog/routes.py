013f03ccbbc2d2b05ea8d19e36571b9b614294a5
Arihant Jain <jaina33@cardiff.ac.uk>
2022-01-26 02:41:53 +0000

    Home page

--- blog/routes.py
+++ blog/routes.py
@@ -12,9 +12,6 @@ def home():
   posts=Post.query.all()
   return render_template('home.html',posts=posts)
 
-@app.route("/about")
-def about():
-  return render_template('about.html', title='About')
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
--- db_fill.py
+++ db_fill.py
@@ -6,11 +6,11 @@ def add_post(post):
     db.session.commit()
 
 
-title = "This is the first post"
+title = "This is the fourth post"
 content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 summary ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt id aliquet risus feugiat in ante metus dictum at. Vitae suscipit tellus mauris a diam maecenas sed enim."
 image_file ="default.jpg" 
-author_id = 1
+author_id = 2
 post = Post(title = title, content = content, summary = summary, image_file = image_file, author_id = author_id)
 add_post(post)
 
